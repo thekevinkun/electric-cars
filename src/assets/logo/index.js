41ed7f76e003184d4a3fa651736a5a2aa6253c0c
thekevinkun <developer.kevinkun@gmail.com>
2024-11-11 23:44:02 +0800

    Add cars logo

---
+++ src/assets/logo/index.js
@@ -0,0 +1,25 @@
+import logo from "./logo.png";
+import audiLogo from "./audi-logo.svg";
+import bmwLogo from "./bmw-logo.svg";
+import chevroletLogo from "./chevrolet-logo.svg";
+import fordLogo from "./ford-logo.svg";
+import hyundaiLogo from "./hyundai-logo.svg";
+import maseratiLogo from "./maserati-logo.svg";
+import mercedesLogo from "./mercedes-logo.svg";
+import porscheLogo from "./porsche-logo.svg";
+import teslaLogo from "./tesla-logo.svg";
+import volkswagenLogo from "./volkswagen-logo.svg";
+
+export {
+  logo,
+  audiLogo,
+  bmwLogo,
+  chevroletLogo,
+  fordLogo,
+  hyundaiLogo,
+  maseratiLogo,
+  mercedesLogo,
+  porscheLogo,
+  teslaLogo,
+  volkswagenLogo,
+};
