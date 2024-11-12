44366b88fa6c8e7be3c7dd6f3a3129f08a6fe03a
thekevinkun <developer.kevinkun@gmail.com>
2024-11-12 09:15:17 +0800

    Added Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,22 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="w-full padding-x sm:py-3 pt-8 pb-2">
+      <ul className="flex flex-wrap flex-col sm:flex-row items-center justify-center">
+        {footerLinks.map((item) => (
+          <li className="lg:px-3 md:px-2 px-1 sm:py-0 py-1">
+            <a
+              href={item.url}
+              className="font-code font-medium md:text-sm text-xs text-slate-zinc"
+            >
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
