a1af0270c933ad7a2245ac55fcc9daab3e5aece0
thekevinkun <developer.kevinkun@gmail.com>
2024-11-12 12:16:30 +0800

    Modify Header and Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,26 @@ import { navigation } from "../constants";
 
 import { RxCross2 } from "react-icons/rx";
 
-const Navbar = ({ addStyle, openNavigation, toggleNavigation }) => {
+const Navbar = ({
+  openNavigation,
+  toggleNavigation,
+  openNavigationOnHover,
+  hideNavigationOnHover,
+}) => {
   return (
     <nav
-      className={`${openNavigation ? "slide-bottom absolute top-0" : "hidden"}  
-        w-full lg:block z-50`}
+      className={`${
+        openNavigation
+          ? "slide-bottom lg:hidden absolute top-0"
+          : openNavigationOnHover
+          ? "slide-bottom hidden lg:block lg:absolute lg:top-[56px]"
+          : "hidden"
+      }  
+       w-full lg:z-[60] z-[75]`}
     >
       <div
-        className={`${
-          addStyle && "!bg-black/100"
-        } w-full min-h-screen lg:min-h-10 
-              bg-black/90 lg:bg-transparent flex flex-col items-center justify-center lg:flex-row transition-all duration-700`}
+        className="w-full min-h-screen lg:min-h-10 bg-black/90 lg:bg-white 
+            flex flex-col items-center justify-center lg:flex-row transition-all duration-700"
       >
         <RxCross2
           className="lg:hidden absolute top-5 right-5 text-primary text-3xl cursor-pointer transition-colors duration-200 hover:text-gray-500"
@@ -33,13 +42,14 @@ const Navbar = ({ addStyle, openNavigation, toggleNavigation }) => {
           ))}
         </div>
 
-        <div className="hidden lg:block">
+        <div className="hidden py-8 lg:grid grid-cols-4 gap-10">
           {navigation.map((item) => (
             <a
               key={item.id}
               href={item.url}
-              className="py-3 lg:py-0 lg:px-3 font-code font-semibold 
-                text-primary text-xl lg:text-base transition-colors duration-200 hover:text-gray-500"
+              className="py-3 font-code font-semibold 
+                text-primary lg:text-black text-xl lg:text-base transition-colors duration-200 hover:text-gray-500"
+              onClick={hideNavigationOnHover}
             >
               {item.title}
             </a>
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { Navbar, Button } from "../components";
 
@@ -7,7 +7,9 @@ import { VscAccount } from "react-icons/vsc";
 import { IoMdHelpCircleOutline } from "react-icons/io";
 
 const Header = () => {
+  const ref = useRef(null);
   const [openNavigation, setOpenNavigation] = useState(false);
+  const [openNavigationOnHover, setOpenNavigationOnHover] = useState(false);
 
   const toggleNavigation = () => {
     if (openNavigation) {
@@ -20,6 +22,30 @@ const Header = () => {
       if (typeof window != "undefined" && window.document) {
         document.body.style.overflow = "hidden";
       }
+    }
+  };
+
+  const showNavigationOnHover = () => {
+    setTimeout(() => {
+      setOpenNavigationOnHover(true);
+      // Disables Background Scrolling whilst the SideDrawer/Modal is open
+      if (typeof window != "undefined" && window.document) {
+        document.body.style.overflow = "hidden";
+      }
+    }, 500);
+  };
+
+  const hideNavigationOnHover = () => {
+    if (openNavigationOnHover) {
+      setOpenNavigationOnHover(false);
+      // Unsets Background Scrolling to use when SideDrawer/Modal is closed
+      document.body.style.overflow = "unset";
+    }
+  };
+
+  const handleHoverOut = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      hideNavigationOnHover();
     }
   };
 
@@ -47,25 +73,28 @@ const Header = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("mouseout", handleHoverOut);
 
     return () => window.removeEventListener("scroll", handleScroll);
   });
 
   return (
     <header
+      ref={ref}
       className={`${
         hideNav ? "top-[-100px]" : "top-0"
       } w-full z-50 fixed transition-[top] duration-1000`}
     >
       <div
-        className={`${addStyle && "bg-white"} w-full padding-x py-4
-            flex items-center transition-[background-color] duration-700`}
+        className={`${
+          (addStyle || openNavigationOnHover) && "bg-white"
+        } relative z-[70] w-full padding-x py-3
+          flex items-center transition-[background-color] duration-700`}
       >
         <a
           className={`${
-            addStyle && "!text-black"
-          } font-grotesk text-primary text-lg lg:text-xl 
-              font-extrabold tracking-wider uppercase transition-colors duration-700`}
+            (addStyle || openNavigationOnHover) && "!text-black"
+          } logo-text flex flex-1 transition-colors duration-700`}
           href="/"
         >
           Electric Cars
@@ -73,38 +102,72 @@ const Header = () => {
 
         <Button
           className={`${
-            addStyle && "!bg-black/90 hover:!bg-black/0"
+            addStyle && "!bg-black/90 hover:!bg-black/100"
           } lg:hidden uppercase
-              glassmorphism-menu text-sm text-primary ml-auto transition-all duration-300 hover:bg-black/30`}
+              glassmorphism-menu text-sm text-primary ml-auto transition-all duration-300 hover:bg-black/25`}
           px="px-4"
           onClick={toggleNavigation}
         >
           Menu
         </Button>
 
+        <div className="relative flex-1 lg:inline-flex hidden z-50">
+          <span
+            className={`${
+              (addStyle || openNavigationOnHover) &&
+              "!text-black hover:!bg-black/5"
+            }
+                font-code font-semibold text-primary text-base cursor-pointer
+                py-1 px-3 hover:bg-black/20 rounded-sm transition-[colors,background-color] duration-700`}
+            onMouseOver={showNavigationOnHover}
+          >
+            Vehicles
+          </span>
+        </div>
+
         <div
-          className={`${addStyle && "!text-black"} hidden ml-auto 
-              lg:flex items-center gap-3 text-primary transition-colors duration-700`}
+          className={`${
+            (addStyle || openNavigationOnHover) && "!text-black"
+          } w-fit hidden ml-auto 
+              lg:flex items-center gap-2 text-primary transition-colors duration-700`}
         >
-          <a href="#support">
+          <a
+            href="#support"
+            className={`${
+              (addStyle || openNavigationOnHover) && "hover:!bg-black/5"
+            } p-[2px]
+              hover:bg-black/50 rounded-sm transition-[background-color] duration-700`}
+          >
             <IoMdHelpCircleOutline className="text-2xl" />
           </a>
 
-          <a href="#world">
+          <a
+            href="#world"
+            className={`${
+              (addStyle || openNavigationOnHover) && "hover:!bg-black/5"
+            } p-[2px]
+              hover:bg-black/50 rounded-sm transition-[background-color] duration-700`}
+          >
             <CiGlobe className="text-2xl" />
           </a>
 
-          <a href="account">
+          <a
+            href="account"
+            className={`${
+              (addStyle || openNavigationOnHover) && "hover:!bg-black/5"
+            } p-[4px]
+              hover:bg-black/50 rounded-sm transition-[background-color] duration-700`}
+          >
             <VscAccount className="text-xl" />
           </a>
         </div>
       </div>
 
       <Navbar
-        addStyle={addStyle}
         openNavigation={openNavigation}
-        setOpenNavigation={setOpenNavigation}
         toggleNavigation={toggleNavigation}
+        openNavigationOnHover={openNavigationOnHover}
+        hideNavigationOnHover={hideNavigationOnHover}
       />
     </header>
   );
